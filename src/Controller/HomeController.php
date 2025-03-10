6db6d9221cc59a92e8c377701a2befd1d1b5d286
Garieldas <dainiusrainys99@gmail.com>
2025-03-10 23:56:11 +0200

    Fetched all articles from database and displayed in home page. Also added random class atribute for each load for article cards

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('pages/home.html.twig');
+        $articles = $entityManager->getRepository(Article::class)->findAll();
+        return $this->render('pages/home.html.twig',
+        ['articles' => $articles]);
     }
+
 }
