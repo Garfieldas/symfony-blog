60f48c70c621adcffc52dabb8a3a395a367cd31f
Garieldas <dainiusrainys99@gmail.com>
2025-03-10 23:15:46 +0200

    Removed unnecessary if statement from article fixtures for creating new author

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -11,14 +11,11 @@ class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $author1 = '';
 
-        if(!$author1) {
-            $author1 = new Author();
-            $author1->setName('John');
-            $author1->setLastName('Doe');
-            $manager->persist($author1);
-        }
+        $author1 = new Author();
+        $author1->setName('John');
+        $author1->setLastName('Doe');
+        $manager->persist($author1);
 
         $article1 = new Article();
         $article1->setTitle('Article 1');
@@ -29,14 +26,10 @@ class ArticleFixtures extends Fixture
 
         $manager->persist($article1);
 
-        $author2 = '';
-
-        if(!$author2) {
-            $author2 = new Author();
-            $author2->setName('John');
-            $author2->setLastName('Pork');
-            $manager->persist($author2);
-        }
+        $author2 = new Author();
+        $author2->setName('John');
+        $author2->setLastName('Pork');
+        $manager->persist($author2);
 
         $article2 = new Article();
         $article2->setTitle('Article 2');
@@ -47,14 +40,10 @@ class ArticleFixtures extends Fixture
 
         $manager->persist($article2);
 
-        $author3 = '';
-
-        if(!$author3) {
-            $author3 = new Author();
-            $author3->setName('Arthur');
-            $author3->setLastName('Doel');
-            $manager->persist($author3);
-        }
+        $author3 = new Author();
+        $author3->setName('Arthur');
+        $author3->setLastName('Doel');
+        $manager->persist($author3);
 
         $article3 = new Article();
         $article3->setTitle('Article 3');
