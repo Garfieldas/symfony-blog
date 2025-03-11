91949907fb8be7cf282d9bb2bc5a79543c26c78a
Garieldas <dainiusrainys99@gmail.com>
2025-03-11 22:43:43 +0200

    Replaced entity manager with article repository to fetch all articles. Created single view to display article by its id. Also created construct methods for repositories that helps to get rid off passing them as an argument for each controller function in home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,17 +5,34 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Article;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ArticleRepository;
+use App\Repository\AuthorRepository;
 
 class HomeController extends AbstractController
 {
+
+    private $articleRepository;
+    private $authorRepository;
+    public function __construct(ArticleRepository $articleRepository, AuthorRepository $authorRepository){
+        $this->articleRepository = $articleRepository;
+        $this->authorRepository = $authorRepository;
+    }
     #[Route('/', name: 'home')]
-    public function home(EntityManagerInterface $entityManager): Response
+    public function home(): Response
     {
-        $articles = $entityManager->getRepository(Article::class)->findAll();
+        $articles = $this->articleRepository->findAll();
         return $this->render('pages/home.html.twig',
         ['articles' => $articles]);
+    }
+    #[Route('/article/{id}', name: 'article')]
+    public function article($id): Response
+    {
+        $article = $this->articleRepository->find($id);
+        $authorId = $article->getAuthor();
+        $author = $this->authorRepository->find($authorId);
+
+        return $this->render('pages/article.html.twig',
+        ['article' => $article, 'author' => $author]);
     }
 
 }
