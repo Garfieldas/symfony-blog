4c827c67dc080ef1fdcef35dc80c75d9fc2e4f54
Garieldas <dainiusrainys99@gmail.com>
2025-03-12 23:58:23 +0200

    Edited form types and placeholders inside form builder

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -6,18 +6,35 @@ use App\Entity\Article;
 use App\Entity\Author;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class ArticleFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('paragprah')
-            ->add('imagePath')
-            ->add('coverPath')
+            ->add('title', TextType::class, [
+                'attr' => array(
+                    'placeholder' => 'Write your own title!',
+                )
+            ])
+            ->add('paragprah', TextareaType::class, [
+                'attr' => array(
+                    'placeholder' => 'Write your own paragraph!',
+                    'style' => 'min-height: 30.75em'
+                )
+            ])
+            ->add('imagePath', FileType::class, [
+                'label' => 'Upload your own article image!',
+            ])
+            ->add('coverPath', FileType::class, [
+                'label' => 'Upload your own article cover!',
+            ])
             ->add('author', EntityType::class, [
                 'class' => Author::class,
                 'choice_label' => 'id',
